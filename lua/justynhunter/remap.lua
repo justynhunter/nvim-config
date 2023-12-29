46bd31b1cffbe061e2ace72f731a4f7c411fe414
Justyn Hunter <justynhunter@gmail.com>
2023-12-28 21:52:46 -0600

    comment remaps

--- lua/justynhunter/remap.lua
+++ lua/justynhunter/remap.lua
@@ -1,12 +1,18 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
+-- move line up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
+-- remove whitespace below
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- page down and up
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- next/prev match, center screen
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -15,7 +21,11 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+-- unmap
 vim.keymap.set("n", "Q", "<nop>")
 
+-- ??
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- make executeable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
