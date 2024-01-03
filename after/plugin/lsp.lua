abe8720587bde9abee72854b7875b2469e76d085
Justyn Hunter <justynhunter@gmail.com>
2024-01-02 20:01:48 -0600

    change some keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,7 +20,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "L", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -30,6 +30,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
+
+vim.api.nvim_command("au BufWritePost *.ex lua vim.lsp.buf.format()")
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
--- lua/justynhunter/remap.lua
+++ lua/justynhunter/remap.lua
@@ -3,14 +3,16 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 -- move line up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- remove whitespace below
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- page down and up
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "J", "<C-d>zz")
+vim.keymap.set("n", "K", "<C-u>zz")
 
 -- next/prev match, center screen
 vim.keymap.set("n", "n", "nzzzv")
